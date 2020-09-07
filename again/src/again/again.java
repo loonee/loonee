f98d00a43cd109fec15f7abc7d01058a7b72d4df
loonee <maharjanluniva@hotmail.com>
2020-09-07 16:01:19 -0500

    if else

---
+++ again/src/again/again.java
@@ -0,0 +1,16 @@
+package again;
+
+public class again {
+	public static void main(String args[]) {
+	int a,b;
+	a=1;
+	b=2;
+	if (a>b) {
+		System.out.println("a is greater than b");
+	}
+	if (b>a) {
+		System.out.println("b is greater than a");
+	}
+	}
+
+}
